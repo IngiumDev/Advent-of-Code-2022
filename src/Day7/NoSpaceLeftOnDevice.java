2ee2086c7b294464a0a1170c9fcd3a98fae8537d
SiKay22 <86780096+SiKay22@users.noreply.github.com>
2022-12-07 07:27:55 +0100

    Created class for tree node and added input

--- src/Day7/NoSpaceLeftOnDevice.java
+++ src/Day7/NoSpaceLeftOnDevice.java
@@ -1,5 +1,16 @@
 package Day7;
 
+
+import java.io.BufferedReader;
+
 public class NoSpaceLeftOnDevice {
+    // Method: parseInputFile creates a tree of nodes from the input file
+    // Input: String filename
+    // Output: Node root
+    public static Node parseInputFile(String filename) {
+        // Create a new Node root
+        Node root = new Node("/");
+        // Read the file line by line
+    }
 
 }
---
+++ src/Day7/Node.java
@@ -0,0 +1,22 @@
+package Day7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node
+{
+    private List<Node> children = null;
+    private String value;
+
+    public Node(String value)
+    {
+        this.children = new ArrayList<>();
+        this.value = value;
+    }
+
+    public void addChild(Node child)
+    {
+        children.add(child);
+    }
+
+}
