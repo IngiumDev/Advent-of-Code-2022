3231633ec5d6d28db53712dc97da4ec228802837
IngiumDev <86780096+IngiumDev@users.noreply.github.com>
2022-12-08 11:25:28 +0100

    Solved Part Two

--- src/Day8/TreeTopHouse.java
+++ src/Day8/TreeTopHouse.java
@@ -5,52 +5,105 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-/* --- Day 8: Treetop Tree House ---
-
-The expedition comes across a peculiar patch of tall trees all planted carefully in a grid. The Elves explain that a previous expedition planted these trees as a reforestation effort. Now, they're curious if this would be a good location for a tree house.
-
-First, determine whether there is enough tree cover here to keep a tree house hidden. To do this, you need to count the number of trees that are visible from outside the grid when looking directly along a row or column.
-
-The Elves have already launched a quadcopter to generate a map with the height of each tree (your puzzle input). For example:
-
-30373
-25512
-65332
-33549
-35390
-
-Each tree is represented as a single digit whose value is its height, where 0 is the shortest and 9 is the tallest.
-
-A tree is visible if all of the other trees between it and an edge of the grid are shorter than it. Only consider trees in the same row or column; that is, only look up, down, left, or right from any given tree.
-
-All of the trees around the edge of the grid are visible - since they are already on the edge, there are no trees to block the view. In this example, that only leaves the interior nine trees to consider:
-
-    The top-left 5 is visible from the left and top. (It isn't visible from the right or bottom since other trees of height 5 are in the way.)
-    The top-middle 5 is visible from the top and right.
-    The top-right 1 is not visible from any direction; for it to be visible, there would need to only be trees of height 0 between it and an edge.
-    The left-middle 5 is visible, but only from the right.
-    The center 3 is not visible from any direction; for it to be visible, there would need to be only trees of at most height 2 between it and an edge.
-    The right-middle 3 is visible from the right.
-    In the bottom row, the middle 5 is visible, but the 3 and 4 are not.
-
-With 16 trees visible on the edge and another 5 visible in the interior, a total of 21 trees are visible in this arrangement.
-
-Consider your map; how many trees are visible from outside the grid?
-*/
 public class TreeTopHouse {
     public static void main(String[] args) {
         String filename = "src/Day8/Day8.txt";
         ArrayList<ArrayList<Integer>> map = loadTreeFile(filename);
         int treesVisible = countVisibleTrees(map);
         System.out.println("Trees visible: " + treesVisible);
+        // Part Two
+        System.out.println("Highest Scenic Score: " + findHighestScenicScorePossible(map));
     }
-    // Count the trees that are visible from the outside of the grid
-    // A tree is visible when all the trees in the same row or column are shorter than it
+
+    /**
+     * Find the highest scenic score possible of the forest
+     * A tree's scenic score is found by multiplying together its viewing distance in each of the four directions
+     *
+     * @param map The map of the forest
+     * @return The highest scenic score possible
+     */
+    public static int findHighestScenicScorePossible(ArrayList<ArrayList<Integer>> map) {
+        int highestScenicScore = 0;
+        // For each tree, find the highest scenic score possible, excluding the edges
+        for (int row = 1; row < map.size() - 1; row++) {
+            for (int col = 1; col < map.get(row).size() - 1; col++) {
+                // Calculate the scenic score possible for this tree
+                int scenicScore = calculateScenicScore(map, row, col);
+                // If it's the highest so far, update the highestScenicScore
+                if (scenicScore > highestScenicScore) {
+                    highestScenicScore = scenicScore;
+                }
+            }
+        }
+        return highestScenicScore;
+    }
+
+    /**
+     * Calculate the scenic score for a tree at a given position
+     * To measure the viewing distance from a given tree, look up, down, left, and right from that tree; stop if you reach an edge or at the first tree that is the same height or taller than the tree under consideration.
+     * (If a tree is right on the edge, at least one of its viewing distances will be zero.)
+     * The scenic score for a tree is the product of its viewing distances in each of the four directions.
+     *
+     * @param map The map of the forest
+     * @param x   The x coordinate of the tree
+     * @param y   The y coordinate of the tree
+     * @return The scenic score of the tree at (x, y)
+     */
+    public static int calculateScenicScore(ArrayList<ArrayList<Integer>> map, int x, int y) {
+        int treeHeight = map.get(x).get(y);
+        int upScore = 0;
+        int downScore = 0;
+        int leftScore = 0;
+        int rightScore = 0;
+        // Look up
+        for (int i = x - 1; i >= 0; i--) {
+            upScore++;
+            if (map.get(i).get(y) >= treeHeight) {
+                break;
+            }
+
+        }
+        // Look left
+        for (int i = y - 1; i >= 0; i--) {
+            leftScore++;
+            if (map.get(x).get(i) >= treeHeight) {
+                break;
+            }
+
+        }
+        // Look down
+        for (int i = x + 1; i < map.size(); i++) {
+            downScore++;
+            if (map.get(i).get(y) >= treeHeight) {
+                break;
+            }
+
+        }
+        // Look right
+        for (int i = y + 1; i < map.get(x).size(); i++) {
+            rightScore++;
+            if (map.get(x).get(i) >= treeHeight) {
+                break;
+            }
+
+        }
+        // A tree's scenic score is found by multiplying together its viewing distance in each of the four directions.
+        return upScore * downScore * leftScore * rightScore;
+    }
+
+    /**
+     * Count the number of trees visible from outside the forest
+     * A tree is visible when all the trees in the same row or column are shorter than it
+     * That can be in any direction i.e. if a tree is visible in one direction, we consider it visible
+     *
+     * @param map The map of the forest
+     * @return The number of trees visible from outside the forest
+     */
     public static int countVisibleTrees(ArrayList<ArrayList<Integer>> map) {
         int count = 0;
         for (int i = 1; i < map.size() - 1; i++) {
             for (int j = 1; j < map.get(i).size() - 1; j++) {
-                if(isTreeVisible(map, i, j)) {
+                if (isTreeVisible(map, i, j)) {
                     count++;
                 }
             }
@@ -59,8 +112,17 @@ public class TreeTopHouse {
         count += map.size() * 2 + map.get(0).size() * 2 - 4;
         return count;
     }
-    // Method isTreeVisible
-    // A tree is visible when all the trees in the same row or column are shorter than it
+
+    /**
+     * Check if a tree is visible from outside the forest
+     * A tree is visible when all the trees in the same row or column are shorter than it
+     * That can be in any direction i.e. if a tree is visible in one direction, we consider it visible
+     *
+     * @param map The map of the forest
+     * @param i   The row of the tree
+     * @param j   The column of the tree
+     * @return True if the tree at (i, j) is visible from outside the forest
+     */
     public static boolean isTreeVisible(ArrayList<ArrayList<Integer>> map, int i, int j) {
         int treeHeight = map.get(i).get(j);
         // A tree is visible if it is taller than all the trees either above or below it or to the left or right of it
@@ -108,15 +170,21 @@ public class TreeTopHouse {
         }
         return visible;
     }
-    // Load in the input file into a 2D ArrayList of Integers
+
+    /**
+     * @param filename The name of the file to read
+     * @return The map of the forest as a 2D ArrayList of Integers
+     */
     public static ArrayList<ArrayList<Integer>> loadTreeFile(String filename) {
         ArrayList<ArrayList<Integer>> treeMap = new ArrayList<>();
         // load the file
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
             String line;
             while ((line = br.readLine()) != null) {
+                // process the line.
                 ArrayList<Integer> row = new ArrayList<>();
                 for (int i = 0; i < line.length(); i++) {
+                    // Add the height of the tree to the row
                     row.add(Integer.parseInt(line.substring(i, i + 1)));
                 }
                 treeMap.add(row);
