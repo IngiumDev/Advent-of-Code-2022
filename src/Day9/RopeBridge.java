0fc76cb9a75cbf7a067ae2a9f3cc5bd7527ef136
IngiumDev <86780096+IngiumDev@users.noreply.github.com>
2022-12-09 07:55:37 +0100

    Created method to parse the input file for day 9

--- src/Day9/RopeBridge.java
+++ src/Day9/RopeBridge.java
@@ -1,5 +1,27 @@
 package Day9;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class RopeBridge {
 
+    /**
+     * @param filename The name of the file to read
+     * @return An ArrayList of Strings which is the list of instructions
+     */
+    public static ArrayList<String[]> parseInputFile(String filename) {
+        ArrayList<String[]> input = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] split = line.split(" ");
+                input.add(split);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return input;
+    }
 }
